70d108645cefb3e503171291b5f3514562786f97
JRJ <jrjensen14@gmail.com>
2021-04-17 14:11:32 -0500

    service workers & web manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,83 +1,62 @@
-// create variable to hold db connection
-let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
-const request = indexedDB.open('budget', 1);
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-  // save a reference to the database 
-  const db = event.target.result;
-  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-  db.createObjectStore('pending', { autoIncrement: true });
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
-// upon a successful 
-request.onsuccess = function(event) {
-  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-  db = event.target.result;
+request.onsuccess = ({ target }) => {
+  db = target.result;
 
-  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  // check if app is online before reading from db
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
     checkDatabase();
   }
 };
 
 request.onerror = function(event) {
-  // log error here
-  console.log(event.target.errorCode);
+  console.log("Woops! " + event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new pizza and there's no internet connection
 function saveRecord(record) {
-  // open a new transaction with the database with read and write permissions 
-  const transaction = db.transaction(['pending'], 'readwrite');
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
 
-  // access the object store for `new_pizza`
-  const store = transaction.objectStore('pending');
-
-  // add record to your store with add method
   store.add(record);
 }
 
-function uploadDatabase() {
-  // open transaction on your db
-  const transaction = db.transaction(['pending'], 'readwrite');
-
-  // access your object store
-  const store = transaction.objectStore('pending');
-
-  // get all records from store and set to a variable
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
   const getAll = store.getAll();
-  
+
   getAll.onsuccess = function() {
-    // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
         }
       })
-        .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-          // open one more transaction
-          const transaction = db.transaction(['pending'], 'readwrite');
-          // access the new_pizza object store
-          const store = transaction.objectStore('pending');
-          // clear all items in your store
-          store.clear();
-
-          alert('All saved pizza has been submitted!');
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      .then(response => {        
+        return response.json();
+      })
+      .then(() => {
+        // delete records if successful
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
     }
   };
 }
